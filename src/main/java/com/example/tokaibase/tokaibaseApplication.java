b5595fa494201679c079013853add73dd26ec030
tatami8810 <kabasinsi@gmail.com>
2025-10-20 22:22:45 +0900

    Initial commit for tokaibase

---
+++ src/main/java/com/example/tokaibase/tokaibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokaibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tokaibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(tokaibaseApplication.class, args);
+	}
+
+}
