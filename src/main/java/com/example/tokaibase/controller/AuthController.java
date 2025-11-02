b3ca4c46beb0ee3a0c5767093fe101cac99d3e9c
tatami8810 <kabasinsi@gmail.com>
2025-11-02 13:41:02 +0900

    8回目のコミット

--- src/main/java/com/example/tokaibase/controller/AuthController.java
+++ src/main/java/com/example/tokaibase/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.tokaibase.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.tokaibase.form.SignupForm;
 
 @Controller
 public class AuthController {
@@ -9,5 +12,12 @@ public class AuthController {
     public String login() {
         return "auth/login";
     }
+    
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("signupForm", new SignupForm());
+        return "auth/signup";
+    }  
+    
 }
 
